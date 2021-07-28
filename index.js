dce84a744f438a2542fb1feee839778ebf652336
Kinfemichael A. Desse <44194230+raskinfe@users.noreply.github.com>
2021-07-28 12:41:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-	function openTab(evt, tabName) {
-  // Declare all variables
-  var i, content, tablinks;
+	function displayTab(evt, tabName) {
+  
+  var i, content, tabtags;
 
-  // Get all elements with class="content" and hide them
+  
   content = document.getElementsByClassName("content");
   for (i = 0; i < content.length; i++) {
     content[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  
+  tabtags = document.getElementsByClassName("tabtags");
+  for (i = 0; i < tabtags.length; i++) {
+    tabtags[i].className = tabtags[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
+  
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 
