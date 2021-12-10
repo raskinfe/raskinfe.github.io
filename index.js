6c051e05506137fd5a38511e22c574b89ac0ef59
Kinfemichael A. Desse <44194230+raskinfe@users.noreply.github.com>
2021-12-10 11:24:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('#header .hamburger');
 const mobile_menu  = document.querySelector('#header .nav-list ul');
 const header = document.querySelector('.header.container');
+const links  = document.querySelectorAll('#header .nav-list ul a');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
@@ -13,6 +14,12 @@ document.addEventListener('scroll', ()=>{
         header.style.backgroundColor = "#29323c";
     } else {
         header.style.backgroundColor = 'transparent';
-
     }
 })
+
+links.forEach(element => {
+    element.addEventListener('click', ()=>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    })
+});
